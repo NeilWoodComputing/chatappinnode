7a2050b022ed66fcba9582c640829f27fbda4cf4
Neilio-PC\Neilio <neil.wood.computing@gmail.com>
2018-02-22 22:47:13 +0000

    updates

--- public/sw.js
+++ public/sw.js
@@ -3,3 +3,15 @@
 self.addEventListener("fetch", function(event){
 
 });
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open("servicecache").then(function(cache) {
+      return cache.addAll(
+        [
+          '/index.ejs'
+        ]
+      );
+    })
+  );
+});
